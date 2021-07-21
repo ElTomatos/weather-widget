850414051963605d0eeda8661fb706aa7e23bef0
Sergey Petrov [CPA] <sa.petrov@alfaleads.net>
2021-07-21 14:37:09 +0300

    added dnd touch backed

--- src/components/WeatherWidgetSettings/WeatherWidgetSettingsDropdown/index.tsx
+++ src/components/WeatherWidgetSettings/WeatherWidgetSettingsDropdown/index.tsx
@@ -5,11 +5,6 @@ import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
-import MultiBackend, {
-  MouseTransition,
-  TouchTransition,
-} from "react-dnd-multi-backend";
-import HTML5toTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch";
 
 /**
  * Components
@@ -37,25 +32,6 @@ type TProps = {
 
 const dndBackend = isTouchDevice() ? TouchBackend : HTML5Backend;
 
-const CustomHTML5toTouch = {
-  backends: [
-    {
-      backend: HTML5Backend,
-      transition: MouseTransition,
-      // by default, will dispatch a duplicate `mousedown` event when this backend is activated
-    },
-    {
-      backend: TouchBackend,
-      // Note that you can call your backends with options
-      options: { enableMouseEvents: true },
-      preview: true,
-      transition: TouchTransition,
-      // will not dispatch a duplicate `touchstart` event when this backend is activated
-      skipDispatchOnTransition: true,
-    },
-  ],
-};
-
 /**
  * Expo
  */
@@ -74,7 +50,7 @@ const WeatherWidgetSettingsDropdown: React.FC<TProps> = ({
   return (
     <div className={classes}>
       {/* @ts-ignore */}
-      <DndProvider backend={MultiBackend} options={CustomHTML5toTouch}>
+      <DndProvider backend={dndBackend}>
         <WeatherWidgetSettingsList
           widgets={widgets}
           onReorder={onReorder}
